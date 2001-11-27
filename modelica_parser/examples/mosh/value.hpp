68563f7b07099bce72909c426c6645a9fa377cb4
ankar <ankar@f25d12d1-65f4-0310-ae8a-bbce733d8d8e>
2001-11-27 22:45:11 +0000

    Added first version of mosh (Modelica shell).
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@648 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

---
+++ modelica_parser/examples/mosh/value.hpp
@@ -0,0 +1,86 @@
+#ifndef VALUE_HPP_
+#define VALUE_HPP_
+
+#include <vector>
+#include <string>
+#include <iostream>
+
+class value
+{
+public:
+  value();
+  value(double val);
+  value(bool val);
+  value(int val);
+
+  virtual ~value();
+
+  //  value(const value& val);
+  
+  
+  void set_value(std::string val);
+  void set_value(int val);
+  void set_value(double val);
+  void set_value(bool val);
+
+  std::string get_string() const;
+  double get_real() const;
+  int get_integer() const;
+  bool get_boolean() const;
+  
+  enum type_en {str,integer,real,boolean,array};
+  type_en type() const;
+  void set_type(type_en type);
+
+  bool is_numeric() const;
+  bool is_real() const;
+  bool is_integer() const;
+  bool is_boolean() const;
+  bool is_array() const;
+
+  void append_to_array(const value& val);
+
+  const value& operator+= (const value& val);
+  value operator+(const value& v) const;
+
+  const value& operator-= (const value& val);
+  value operator-(const value& val) const;
+
+  const value& operator*= (const value& val);
+    value operator*(const value& val) const;
+    
+  const value& operator/= (const value& val);
+  value operator/(const value& val) const;
+  
+  value operator- () const;
+  
+  friend ostream& operator<< (ostream& o, const value& v);
+  
+  friend const value power(const value& x, const value& y);
+  friend const value not_bool(const value& x);
+  friend const value and_bool(const value& x, const value& y);
+  friend const value or_bool(const value& x, const value& y);
+  friend const value modelica_if(const value& x, const value& y, const value& z);
+  //    friend const value less(const value& x, const value& y);
+  friend const value lesseq(const value& x, const value& y);
+  //  friend const value greater(const value& x, const value& y);
+  friend const value greatereq(const value& x, const value& y);
+  friend const value eqeq(const value& x, const value& y);
+  friend const value lessgt(const value& x, const value& y);
+
+  friend const value create_array(const value& x);
+  friend const value create_array(const value& x, const value& y);
+  friend const value create_array(const value& x, const value& y, const value& z);
+    
+private:
+  std::string m_string;
+  int m_integer;
+  double m_real;
+  bool m_boolean;
+  type_en m_type;
+  
+  double to_double() const;
+  std::vector<value> m_array;
+    
+};
+#endif
