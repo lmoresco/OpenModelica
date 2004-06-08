9b27eecc43f004965796e0cd19e0e01ecafc779a
haklu <haklu@f25d12d1-65f4-0310-ae8a-bbce733d8d8e>
2004-06-08 09:35:04 +0000

    Changed to multithreaded libraries and made separate configurations for building with mico statically linked and without mico all together. Also added bat-files for make clean and rebuild in MakeRMLandGRAMMAR
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@1184 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

---
+++ modeq/VC7/rebuildmodeq.bat
@@ -0,0 +1,4 @@
+@echo off
+cd > c:\cygwin\tmp\path.txt
+c:\cygwin\bin\bash --login -c "cd `cygpath -f /tmp/path.txt`/../..;make clean" 
+c:\cygwin\bin\bash --login -c "cd `cygpath -f /tmp/path.txt`/../..;make vctarget" 
