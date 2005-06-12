084c52afc92eff85c0c120476938cbfd83d764d9
adrpo <adrpo@f25d12d1-65f4-0310-ae8a-bbce733d8d8e>
2005-06-12 20:42:15 +0000

    Adrian Pop, adpro@ida.liu.se 2005-06-12 added the relation: RML.debug_print("var_name", var) => () that can print ANY value. Use it for debugging!
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@1808 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- Compiler/VC7/rmlRuntime/runtime/common/misc-print.c
+++ Compiler/VC7/rmlRuntime/runtime/common/misc-print.c
@@ -9,3 +9,80 @@ RML_BEGIN_LABEL(RML__print)
     RML_TAILCALLK(rmlSC);
 }
 RML_END_LABEL
+
+RML_BEGIN_LABEL(RML__debug_5fprint)
+{
+	void *str = rmlA0;
+	/*if (execution_type == RMLDB_STEP || rmldb_show == RMLDB_SHOW)*/
+	{
+		printf ("%s=[", RML_STRINGDATA(str));
+		rmldb_var_print(rmlA1);
+		printf ("]\n");
+	}
+	RML_TAILCALLK(rmlSC);
+}
+RML_END_LABEL
+
+void rmldb_var_print(void *p)
+{
+	/* printf("[%p]", p); */
+	if (!p) { printf ("NIL"); fflush(stdout); return; }
+	if( RML_ISIMM(p) ) 
+	{
+		printf ("%d", RML_UNTAGFIXNUM(p));    
+	} 
+	else 
+	{
+		rml_uint_t phdr = RML_GETHDR(p);            
+		if( phdr == RML_REALHDR ) 
+		{
+			printf ("%f", rml_prim_get_real(p));
+			fflush(stdout);
+		} else 
+			if( RML_HDRISSTRING(phdr) ) 
+			{
+				printf ("\"%s\"", RML_STRINGDATA(p));
+				fflush(stdout);
+				/* use if neccesarry RML_HDRSTRLEN(phdr) */
+			} else 
+				if( RML_HDRISSTRUCT(phdr) ) 
+				{
+					void **pp = RML_STRUCTDATA(p);
+
+					rml_uint_t slots = RML_HDRSLOTS(phdr);
+					rml_uint_t constr = RML_HDRCTOR(phdr);
+					rml_uint_t sl = slots;
+					if (slots == 0)
+					{
+						printf ("{S(%d)[%d]=NIL}", constr, slots);
+						fflush(stdout);
+						return;
+					}
+					
+					printf ("S(%d)[%d](", constr, slots);
+
+					fflush(stdout);
+					// function definition
+					if ((constr == 64 || constr==13) &&
+						slots > 1000000) return;
+					if( slots != 0 )
+					{
+						// printf ("\n\t"); 
+						while( --slots > 0 )
+						{
+							printf("%d=", sl-slots); 
+							rmldb_var_print(*pp++);
+							printf (",");
+							fflush(stdout);
+						}
+						p = *pp; 
+						rmldb_var_print(*pp); printf (")"); fflush(stdout);
+						// goto tail_recur_debug;  
+					}					    
+				} 
+				else 
+				{
+					printf ("UNKNOWN"); fflush(stdout);
+				}
+	}
+}
