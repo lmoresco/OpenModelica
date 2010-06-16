f1abd5c73e07306af689e332e97d0c97d3cc8e7f
perost <perost@f25d12d1-65f4-0310-ae8a-bbce733d8d8e>
2010-06-16 08:45:04 +0000

    - Added an alternate, more parallel way of running the testsuite. See comments   in testsuite/partest/runtests.pl for instructions on how to use it.
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@5674 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

---
+++ testsuite/partest/runtest.pl
@@ -0,0 +1,150 @@
+#!/usr/bin/perl
+
+# Author: Per Östlund
+#
+# This script runs on testcase, usually given to it by the runtests.pl script.
+
+use strict;
+use warnings;
+use Cwd;
+use Term::ANSIColor;
+use File::Path qw(rmtree);
+use feature "switch";
+
+# Get the testcase to run from the command line argument.
+my $test_full = $ARGV[0];
+# Extract the directory and test name.
+(my $test_dir, my $test) = $test_full =~ /(.*)\/([^\/]*)$/;
+# Build the full path to the temporary directory to run the test in.
+my $tmp_path_full = $test_dir . "/" . $test . "_temp";
+
+# Makes a symbolic link to a file.
+sub make_link {
+	my $file = shift;
+
+  # Depending on how the path is given we need to use different rules for how
+  # the symlink should be created.
+	given($file) {
+		when (/\.\.\/([\w-]*)\//) { symlink("../" . $1, "../" . $1); }
+		when (/^(\w*)\/(.*)/) 	  { symlink("../" . $1, $1); }
+		when (/(.*)/)						  { symlink("../" . $1, $1); }
+		default 								  { symlink("../" . $file, $file); }
+	}
+}
+
+# Some tests use libraries that we need to symlink the corresponding heards for.
+sub lib_to_header {
+	my $lib = shift;
+
+	given($lib) {
+		when (/lib(\w*)\.\w*/) 	{ return $1 . ".h"; }
+		when (/(\w*)\.lib/) 		{ return $1 . ".h"; }
+		default { return ""; }
+	}
+}
+
+# Some tests needs some special symlinks that are hard to determine by just
+# parsing the test-scripts, so this functions applies a couple of special rules.
+sub make_test_specific_links {
+	given($test) {
+		when ("ExtObj.mos") { make_link("testTables.txt"); }
+		when ("ModelicaUtilities.mos") { 
+			make_link("ModelicaUtilities.myExtFunction.c"); 
+		}
+		when ("Identifier.mos") { make_link("checkparseerror.c"); }
+		when ("ExternalFunctionResultOrder.mos") {
+			make_link("ext_ExternalFunctionResultOrder.c");
+		}
+		when ("PartialFn6.mos") { make_link("PartialFn6.ext_f.c"); };
+	}
+}
+
+# This functions sets up a sandbox for a tests by creating a temporary directory
+# and symlinking the needed files into it.
+sub enter_sandbox {
+	mkdir($tmp_path_full);
+	chdir($tmp_path_full);
+	make_link($test);
+	make_test_specific_links();
+
+  # Parse the testscript to see if it has any special requirements.
+	open(my $in, "<", $test) or die "Couldn't open $test: $!\n";
+
+	my $stop_expr;
+
+  # If we are parsing a mos-file, stop when we reach 'Result:'. Otherwise, parse
+  # until we reach a line that's not a comment.
+	if(substr($test, -3) eq "mos") {
+		$stop_expr = "// Result:";
+	} else { 
+		$stop_expr = "^[^/]";
+	}
+
+  # Check for a couple of keywords such as loadFile, and create the neccessary
+  # symlinks.
+	while(<$in>) {
+		given($_) {
+			when (/$stop_expr/) 							{ last; }
+			when (/setup_command.*\s(.*\.c)/) { make_link($1); }
+			when (/loadFile.*\(\"(.*)\"\)/) 	{ make_link($1); }
+			when (/system\(\"(gcc|g\+\+).*\s(\w*\.\w*)\s(\w*\.\w*)/) {
+				my $header = lib_to_header($2);
+				make_link($header); 
+				make_link($3);
+			}
+			when (/system\(\"(gcc|g\+\+).*\s(\w*\.\w*)/) {
+				make_link($2);
+			}	
+			when (/external.*\\\"(.*\.h)\\\"/){
+				make_link($1);
+
+				while(<$in>) {
+					if(/end (.*);/) {
+						make_link($1 . ".c");
+						last;
+					}
+				}
+			}
+		}
+	}
+}
+
+# Exit the sandbox by going up one directory level and delete the temporary
+# directory.
+sub exit_sandbox {
+	chdir("..");
+  rmtree($test . "_temp");
+}
+
+
+enter_sandbox();
+
+# Determine the full path to rtest.
+my $rtest = "../" x ($test_full =~ tr/\///) . "rtest -v ";
+# If we're in meta, append the MetaModelica flag to rtest.
+$rtest = $rtest . " +g=MetaModelica " if $test_dir eq "./meta";
+
+# Run the testscript and redirect output to a logfile.
+system("$rtest $test &> $test.test_log");
+
+# Read the logfile and see if the test succeeded or failed.
+open(my $test_log, "<", "$test.test_log") or die "Couldn't open test log $test.log: $!\n";
+
+my $exit_status = 1;
+
+while(<$test_log>) {
+	if(/== (\d) out of 1 tests failed.*time: (\d*)/) {
+		my $time = $2;
+		if($1 =~ /0/) {
+			print color 'green';
+		} else {
+			print color 'red';
+			$exit_status = 0;
+		}
+		print " [$test:$time]";
+    print color 'reset';
+	}
+}
+
+exit_sandbox();
+exit $exit_status;
---
+++ testsuite/partest/runtests.pl
@@ -0,0 +1,157 @@
+#!/usr/bin/perl
+
+# Author: Per Östlund
+#
+# This script parses the makefiles in the testsuite and extracts (almost) all
+# testcases. It then runs each test with the runtest.pl script, which creates a
+# new directory for each test and symlinks all needed files into that directory.
+# In this way it's possible to run all tests in parallel, which greatly speeds
+# up testing when using more than two processor cores.
+#
+# Usage: Run without any arguments to run the whole testsuite, or with -f to
+#        run a fast test, i.e. skipping the libraries directory.
+#
+# NOTE: This is not the official OpenModelica way of running the testsuite, so
+#       you should run a 'make test' before committing any changes just to make
+#       sure that this script doesn't miss anything.
+#
+# NOTE: This script has only been tested on Linux so far, and will probably not
+#       work under any other platform without some modifications.
+#
+# TODO: MetaModelicaDev in meta is not run yet, since those tests are organized
+#       a bit differently.
+
+use strict;
+use warnings;
+
+use threads;
+use threads::shared;
+use Thread::Queue;
+use Term::ANSIColor;
+use List::Util 'shuffle';
+use Cwd;
+
+my $fast = 0;
+my @test_list;
+my $test_queue = Thread::Queue->new();
+my $tests_failed :shared = 0;
+my @failed_tests :shared;
+my $testscript = cwd() . "/runtest.pl";
+
+# Parse a makefile
+sub read_makefile {
+	my $dir = shift;
+
+	return if $dir eq "./java"; # Skip the java tests, since they don't work.
+  return if($fast == 1 and $dir eq "./libraries"); # Skip libraries if -f is given.
+
+	open(my $in, "<", "$dir/Makefile") or die "Couldn't open $dir/Makefile: $!";
+
+	while(<$in>) {
+		if(/(\S+) -f Makefile test/) {  # Recursively parse makefiles.
+			read_makefile("$dir/$1");
+		}
+		elsif(/^TESTFILES.*=.*$/) { # Found a list of tests, parse them.
+			seek($in, -length($_), 1);
+			parse_testfiles($in, $dir);
+		}
+	}
+}	
+
+# Parse a list of tests given in a makefile by TESTFILES.
+sub parse_testfiles {
+	my $in = shift;
+	my $path = shift;
+
+	while(<$in>) {
+		add_tests($_, $path) unless /#.*/; # Skip lines beginning with #
+		last unless /\\/; # If the line doesn't end with \, stop.
+	}
+}
+
+# Extract all files beginning with .mo|.mof|.mos from a line.
+sub add_tests {
+	my @tests = split(/\s|=|\\/, shift);
+	my $path = shift;
+
+	@tests = grep(/\.mo|\.mof|\.mos/, @tests);
+	@tests = map { $_ = "$path/$_" } @tests; 
+
+  push @test_list, @tests;
+}
+
+# Run the tests by dequeuing them from the list of tests and calling the
+# runtest.pl script.
+sub run_tests {
+	while(defined(my $test_full = $test_queue->dequeue_nb())) {
+		(my $test_dir, my $test) = $test_full =~ /(.*)\/([^\/]*)$/;
+		system("$testscript $test_full");
+    if($? >> 8 == 0) { # Add the test to the list of failed tests if it failed.
+      lock($tests_failed);
+      $tests_failed++;
+      lock(@failed_tests);
+      push @failed_tests, $test_full;
+    }
+	}
+}
+
+# Check for the -f flag.
+for(@ARGV){
+  if(/-f/) {
+    $fast = 1;
+  }
+}
+
+# Assume that we are in a subdirectory of the testsuite, so go up one level and
+# parse the makefile there.
+chdir("..");
+read_makefile(".");
+
+# Reverse the list of tests, because I prefer to have mofiles and mosfiles run
+# before the much slower libraries tests.
+@test_list = reverse(@test_list);
+foreach(@test_list) {
+	$test_queue->enqueue($_);
+}
+
+# Default is two threads.
+my $thread_count = 2;
+
+# Check if we can open /proc/cpuinfo to see how many cores are available, and
+# use that many threads instead.
+if(open(my $in, "<", "/proc/cpuinfo")) {
+	$thread_count = 0;
+
+	while(<$in>) {
+		$thread_count++ if /processor/;
+	}
+	print "$thread_count threads\n";
+}
+
+# Make sure that omc-diff is generated before trying to run any tests.
+system("make -C difftool &> /dev/null");
+
+# Run the tests by calling the run_tests function with multiple threads.
+for(my $i = 0; $i < $thread_count; $i++) {
+	threads->create(\&run_tests);
+}
+
+# Wait for the tests to finish.
+foreach my $thr (threads->list()) {
+	$thr->join();
+}
+
+# Print out the list of tests that failed, and a summary of how many failed.
+print color 'reset';
+print "\n";
+
+if(@failed_tests) {
+  print "\nFailed tests:\n";
+
+  foreach my $failed_test (@failed_tests) {
+    print "\t" . $failed_test . "\n";
+  }
+}
+
+my $test_count = @test_list;
+print "\n$tests_failed of $test_count failed\n";
