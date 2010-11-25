9d8d94b1af7543b49f61663d91f48602188b386b
sjoelund.se <sjoelund.se@f25d12d1-65f4-0310-ae8a-bbce733d8d8e>
2010-11-25 17:36:56 +0000

    - Change MetaModelica try/throw/catch to setjmp/longjmp implementation as it gives a 3-10x speedup
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@7190 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- testsuite/bootstrapping/main.c
+++ testsuite/bootstrapping/main.c
@@ -1,15 +1,18 @@
 #include "Main_main.h"
 
+int rml_execution_failed()
+{
+  fflush(NULL);
+  fprintf(stderr, "Execution failed!\n");
+  return 1;
+}
+
 int main(int argc, char **argv) {
   void *lst = mmc_mk_nil();
   for (int i=argc-1; i>0; i--)
     lst = mmc_mk_cons(mmc_mk_scon(argv[i]), lst);
-  try {
+  MMC_TRY_TOP()
     _Main_main(lst);
-  } catch (...) {
-    fflush(NULL);
-    fprintf(stderr, "Execution failed!\n");
-    return 1;
-  }
+  MMC_CATCH_TOP(return rml_execution_failed())
   return 0;
 }
