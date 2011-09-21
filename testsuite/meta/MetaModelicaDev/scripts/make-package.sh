d10a4a40d55a2bbdcf4ea388665c62a7ad08512e
sjoelund.se <sjoelund.se@f25d12d1-65f4-0310-ae8a-bbce733d8d8e>
2011-09-21 08:46:43 +0000

    - Updated make-package script
    git-svn-id: https://openmodelica.org/svn/OpenModelica/trunk@9887 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- testsuite/meta/MetaModelicaDev/scripts/make-package.sh
+++ testsuite/meta/MetaModelicaDev/scripts/make-package.sh
@@ -14,7 +14,7 @@ svn export .. $DIR/MetaModelica
   cp "../documentation/`echo $f | sed 's/ppt$/pdf/'`" $DIR/MetaModelica/documentation/ || exit 1
 done)
 cp ../../../../doc/OpenModelicaMetaProgramming.pdf $DIR/MetaModelica/documentation/ || exit 1
-wget "http://liu.diva-portal.org/smash/get/diva2:418188/FULLTEXT01" -o $DIR/MetaModelica/documentation/MetaModelica2.0.pdf || exit 1
+wget "http://liu.diva-portal.org/smash/get/diva2:418188/FULLTEXT01" -O $DIR/MetaModelica/documentation/MetaModelica2.0.pdf || exit 1
 
 find $DIR -name SOLUTION*.mo* -delete
 
